8b1397f831b8f3aa0a448c73f9a96b55448ecf5f
Shuai Yuan <iuanshuai@hotmail.com>
2018-05-02 09:29:21 +0800

    UIViewHelper

---
+++ Cards/UIViewHelper.swift
@@ -0,0 +1,63 @@
+//
+//  UIViewHelper.swift
+//  Cards
+//
+//  Created by Shuai Yuan on 5/1/18.
+//  Copyright © 2018 Shuai Yuan. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            return layer.shadowColor != nil ? UIColor(cgColor: layer.shadowColor!) : nil
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+}
